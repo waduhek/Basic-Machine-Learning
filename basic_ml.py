630e429bd040059ad550edf579934c0b428f617b
Ryan Noronha <ryannor56a@gmail.com>
2018-03-09 20:25:44 +0530

    Added comments

--- basic_ml.py
+++ basic_ml.py
@@ -1,10 +1,12 @@
 from sklearn import tree
 
+# 1 for bumpy texture, 0 for smooth texture
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# 1 for orange, 0 for apple
 labels = [1, 1, 0, 0]
 
 clf = tree.DecisionTreeClassifier()
 
 clf = clf.fit(features, labels)
 
-print(clf.predict([[160, 0]]))
+print(clf.predict([[160, 1]]))
